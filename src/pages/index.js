80e7b2fe2829f28be0e8b791b462abfc7d2ada42
Joe Baker <joesbaker@gmail.com>
2018-01-19 12:49:01 +0000

    Add session titles and times

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,32 +2,6 @@ import React, { Component } from 'react'
 import Link from 'gatsby-link'
 import Spreadsheet from 'gatsby-source-google-sheets'
 
-export const query = graphql `query SessionsQuery {
-  allGoogleSheetSessionsRow(filter: {
-    year: {eq: 2018}
-  }) {
-    edges {
-     	node {
-     	  id
-        year
-        day
-        time
-        session
-        capacity
-        title
-        leader1
-        leader2
-        leader3
-        sessionnotes
-        sessionnoteslongurl
-        hashtag
-        twitterconversation
-      }
-    }
-  }
-}
-`
-
 class IndexPage extends Component {
 
   constructor() {
@@ -35,15 +9,31 @@ class IndexPage extends Component {
     this.room = /r(.*)/;
   }
 
+  splitString(string, size) {
+    var re = new RegExp('.{1,' + size + '}', 'g');
+    var match;
+    var elements = new Array();
+    while (match = re.exec(string)) {
+      elements[elements.length] = match[0];
+    }
+    return elements;
+  }
+
   render() {
+    console.log(this.props);
     return (
-      <div className="session-container">
-        <div className="session-time">11:00</div>
-        {this.props.data.allGoogleSheetSessionsRow.edges.map(({node}, index) =>
-          <div className="session">
-            <h2 className="session__title">{node.title}This is a test title, kind of long but not too long in my honest
-              opinion</h2>
-            <p className="session__info"><strong>Room:</strong> {this.room.exec(node.hashtag)[1]}</p>
+      <div className="sessions">
+        {this.props.data.allGoogleSheetSessionsRow.group.map((element, key) =>
+          <div className="session-container" key={key}>
+            <h1>Session {element.fieldValue}</h1>
+            <div className="session-time">{this.splitString(element.edges[0].node.time, 2).join(':')}</div>
+            {element.edges.map(({node}, index) =>
+              <div className="session" key={index}>
+                <h2 className="session__title">{node.title}This is a test title, kind of long but not too long in my honest
+                  opinion</h2>
+                <p className="session__info"><strong>Room:</strong> {this.room.exec(node.hashtag)[1]}</p>
+              </div>
+            )}
           </div>
         )}
       </div>
@@ -52,3 +42,34 @@ class IndexPage extends Component {
 }
 
 export default IndexPage
+
+
+
+export const query = graphql `query Session1Query {
+  allGoogleSheetSessionsRow(filter: {
+    year: {eq: 2018}
+  }) {
+    group (field: session) {
+      fieldValue
+      edges {
+        node {
+          id
+          year
+          day
+          time
+          session
+          capacity
+          title
+          leader1
+          leader2
+          leader3
+          sessionnotes
+          sessionnoteslongurl
+          hashtag
+          twitterconversation
+        }
+      }
+    }
+  }
+}
+`
